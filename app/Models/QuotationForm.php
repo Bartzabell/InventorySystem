7424a061908cba360d8e985ddd1760e4a91e1b2f
Jhenrich <85864804+Bartzabell@users.noreply.github.com>
2025-02-26 17:57:53 +0800

    models

--- app/Models/QuotationForm.php
+++ app/Models/QuotationForm.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuotationForm extends Model
 {
-    //
+    protected $fillable = [
+
+    ];
 }
--- app/Models/RequestForm.php
+++ app/Models/RequestForm.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RequestForm extends Model
 {
-    //
+    protected $fillable = [
+
+    ];
 }
